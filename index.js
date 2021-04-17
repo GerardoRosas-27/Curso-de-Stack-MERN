931a9f0c67c2680a6e111129407c6a852474963d
GerardoRosas-27 <brustergabriel24@gmail.com>
2021-04-17 14:41:43 -0500

    terminada la funcion de actualizar

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function () {
-	getListarUsuarios();
+    getListarUsuarios();
 });
 async function getListarUsuarios() {
     const url = "http://localhost:3000/api/users";
@@ -9,11 +9,11 @@ async function getListarUsuarios() {
     crearFilas(response.data);
 }
 
-function crearFilas(data){
+function crearFilas(data) {
     console.log('entro for: ', data);
     var html = "";
     data.forEach(element => {
-       html = html + '<tr>' + '<td>'+ element.nombre + '</td> <td>'+ element.apellidoPaterno + '</td><td>'+ element.apellidoMaterno + '</td><td>'+ element.telefono + '</td><td>'+ element.correo + '</td><td>'+ element.matricula + '</td><td> <button onclick="onEliminar(this)" data-id="'+ element._id +'" class="btn btn-success">Eliminar</button> </td></tr>';
+        html = html + '<tr>' + '<td>' + element.nombre + '</td> <td>' + element.apellidoPaterno + '</td><td>' + element.apellidoMaterno + '</td><td>' + element.telefono + '</td><td>' + element.correo + '</td><td>' + element.matricula + '</td><td> <button onclick="onEliminar(this)" data-id="' + element._id + '" class="btn btn-danger">Eliminar</button> <button onclick="onEditar(this)" data-id="' + element._id + '" class="btn btn-success">Editar</button> </td></tr>';
     });
     console.log(html)
     $('#contentTR').html(html);
@@ -35,61 +35,103 @@ async function onGuardar() {
     var response = await ajaxData(url, data, "POST");
     console.log("response:", response);
     alert(response.mensaje);
-    getListarUsuarios();  
+    getListarUsuarios();
 }
 
-async function onEliminar(element){
+async function onEliminar(element) {
     var id = $(element).data('id');
     console.log("id: ", id);
-    var url= "http://localhost:3000/api/users/" + id;
+    var url = "http://localhost:3000/api/users/" + id;
     var response = await ajaxDataDelet(url);
     alert(response.mensaje);
-    getListarUsuarios(); 
+    getListarUsuarios();
+}
+async function onEditar(element) {
+    var id = $(element).data('id');
+    var url = "http://localhost:3000/api/users/" + id;
+    var response = await ajaxDataGet(url);
+    $('#nombre').val(response.data.nombre);
+    $('#apellidoPaterno').val(response.data.apellidoPaterno);
+    $('#apellidoMaterno').val(response.data.apellidoMaterno);
+    $('#telefono').val(response.data.telefono);
+    $('#correo').val(response.data.correo);
+    $('#matricula').val(response.data.matricula);
+    $('#contenActualizar').html('<button data-id="' + id + '" onclick="onActualizar(this)" class="btn btn-success">Actualizar</button>');
+}
+
+async function onActualizar(element) {
+    var id = $(element).data('id');
+    var url = "http://localhost:3000/api/users/" + id;
+
+    var data = {
+        "nombre": $('#nombre').val(),
+        "apellidoPaterno": $('#apellidoPaterno').val(),
+        "apellidoMaterno": $('#apellidoMaterno').val(),
+        "telefono": $('#telefono').val(),
+        "correo": $('#correo').val(),
+        "matricula": $('#matricula').val(),
+    }
+    console.log("enviar data:", data);
+    var response = await ajaxData(url, data, "PUT");
+    console.log("response:", response);
+    alert(response.mensaje);
+    limpiar();
+    getListarUsuarios();
+}
+
+function limpiar() {
+    $('#contenActualizar').html('');
+    $('#nombre').val('');
+    $('#apellidoPaterno').val('');
+    $('#apellidoMaterno').val('');
+    $('#telefono').val('');
+    $('#correo').val('');
+    $('#matricula').val('');
 }
 
 async function ajaxData(url, data, tipo) {
-	try {
-		const response = await fetch(url, {
-			method: tipo,
-            headers: {'Content-Type': 'application/json'},
-			body: JSON.stringify(data)
-		});
-		const respuesta = JSON.parse(await response.text());
+    try {
+        const response = await fetch(url, {
+            method: tipo,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+        const respuesta = JSON.parse(await response.text());
         //const respuesta = await response.text();
-		return respuesta;
+        return respuesta;
 
-	} catch (err) {
-		console.log('Error en el ajax:', err);
+    } catch (err) {
+        console.log('Error en el ajax:', err);
         alert(err);
-	}
+    }
 }
 async function ajaxDataGet(url) {
-	try {
-		const response = await fetch(url, {
-			method: 'GET',
-            headers: {'Content-Type': 'application/json'},
-		});
-		const respuesta = JSON.parse(await response.text());
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
+        const respuesta = JSON.parse(await response.text());
         //const respuesta = await response.text();
-		return respuesta;
+        return respuesta;
 
-	} catch (err) {
-		console.log('Error en el ajax:', err);
+    } catch (err) {
+        console.log('Error en el ajax:', err);
         alert(err);
-	}
+    }
 }
 async function ajaxDataDelet(url) {
-	try {
-		const response = await fetch(url, {
-			method: 'DELETE',
-            headers: {'Content-Type': 'application/json'},
-		});
-		const respuesta = JSON.parse(await response.text());
+    try {
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+        });
+        const respuesta = JSON.parse(await response.text());
         //const respuesta = await response.text();
-		return respuesta;
+        return respuesta;
 
-	} catch (err) {
-		console.log('Error en el ajax:', err);
+    } catch (err) {
+        console.log('Error en el ajax:', err);
         alert(err);
-	}
+    }
 }
